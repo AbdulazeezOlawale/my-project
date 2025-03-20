01c358d78ce7368e4df9ef9065a885a0ed470224
AbdulazeezOlawale <orionabq@gmail.com>
2025-03-20 12:57:45 +0100

    first rn app

--- App.js
+++ App.js
@@ -1,21 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, Image, ScrollView } from 'react-native'
+import React from 'react'
+const logoImage = require('./assets/images.jpeg');
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style = {{padding: 20}}>
+      <ScrollView>
+        
+        <Image source={{ uri: "https://stecs.ng/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fhero_img.c10f00dc.webp&w=3840&q=75"}} style={{height: 300, width: 300}}/>
+        <Text style={{fontSize: 22}}>Develop, review, and deploy
+          Learn how to develop by reading the docs in the Develop section. You'll learn how to create UI elements, add unit tests, include native modules, and more.
+
+          Once you've developed your app, you can share it with your teammates for review.
+
+          Finally, you can build and submit your project to the app stores.
+
+          Step-by-step guide
+          For a guided, step-by-step walkthrough of building an app with Expo from start to finish, check out the tutorial.
+
+          Previous (Get started)
+
+          Start developing
+
+          Next (Develop)
+
+          Tools for development
+
+          Was this doc helpful?
+
+
+
+          Share your feedback
+
+          Ask a question on the forums
+
+          Edit this page
+
+          Last updated on May 22, 2024
+          Sign up for the Expo Newsletter
+
+          reader@email.com
+
+          Sign Up
+        </Text>
+        <Image source={{ uri: "https://stecs.ng/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fhero_img.c10f00dc.webp&w=3840&q=75"}} style={{height: 300, width: 300}}/>
+      </ScrollView>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
